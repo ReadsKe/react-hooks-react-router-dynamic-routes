26540cabe30a1f4324f1791fe00a413c31d6dbf6
Samuel Kimote <readssamuel@.com>
2023-11-11 14:36:38 +0300

    complete

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -3,9 +3,15 @@ import { Route } from "react-router-dom";
 import MoviesList from "./MoviesList";
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+      <Route path={`${match.url}/:movieId`}><MovieShow movies={movies} /></Route>
     </div>
   );
 }
